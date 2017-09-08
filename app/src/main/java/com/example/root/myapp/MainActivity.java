a720993104efd8358aa2208072e80ab52e6724cb
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-08 12:58:31 +0300

    I have added the method to increase the number and attached it to the add button

--- app/src/main/java/com/example/root/myapp/MainActivity.java
+++ app/src/main/java/com/example/root/myapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.root.myapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +20,16 @@ public class MainActivity extends AppCompatActivity {
         Toast myToast = Toast.makeText(this, "Hello Toast!",
                 Toast.LENGTH_SHORT);
         myToast.show();
+    }
+    public void countMe(View view){
+        //get the view
+        TextView myNumber=(TextView) findViewById(R.id.textView);
+        //get value of the view
+        String theNumber=myNumber.getText().toString();
+        //convert it into number and increase it
+       Integer finalNumber=Integer.parseInt(theNumber);
+        finalNumber++;
+        //make the view we called to display the number
+        myNumber.setText(finalNumber.toString());
     }
 }
